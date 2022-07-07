a4792389e43e91d2ebc3166fa8f794978c32ad1a
simicodes <simeonoffiong@gmail.com>
2022-07-07 09:53:19 -0600

    Updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,7 +11,7 @@ const Footer = () => {
     <div className="w-full mt-24 bg-slate-900 text-gray-300 py-8 px-2">
       <div className="max-w-[1240px] mx-auto grid grid-cols-2 md:grid-cols-6 border-b-2 border-gray-600 py-8">
         <div>
-          <h6 className="font-bold uppercase pt-2 ">Solutions</h6>
+          <h6 className="font-bold uppercase pt-2">Solutions</h6>
           <ul>
             <li className="py-1">Marketing</li>
             <li className="py-1">Analytics</li>
